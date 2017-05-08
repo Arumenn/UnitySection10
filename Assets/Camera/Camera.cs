c6db5473a0d9796fc4085e96e90840cea1249d8a
arumenn <abel.laflamme@gmail.com>
2017-05-08 14:54:17 -0400

    RollerBall and camera follow

---
+++ Assets/Camera/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public Transform ballToFollow;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //follows the ball
+        Vector3 position = transform.position;
+        position.z = ballToFollow.transform.position.z;
+        transform.position = position;
+	}
+}
